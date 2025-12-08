e0fb6436dfd22aca31b67736c08a88f1bfeeb818
Vasu <vasuperugu6@gmail.com>
2025-12-08 13:30:32 +0530

    init

---
+++ travels/bookings/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Bus, Seat
+
+# Register your models here.
+
+class BusAdmin(admin.ModelAdmin):
+    list_display = ('bus_name', 'number', 'origin', 'destination', 'start_time', 'reach_time', 'no_of_seats', 'price')
+
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ('seat_number', 'bus', 'is_booked')
+
+admin.site.register(Bus, BusAdmin)
+admin.site.register(Seat, SeatAdmin)
